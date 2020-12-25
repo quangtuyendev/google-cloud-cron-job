42d947220fcfa56c1d98e7bf1a2961df3c8210c1
quangtuyendev <quangtuyennguyen09@gmail.com>
2020-12-25 08:15:19 +0700

    [master] upload code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    /* Automatic call this api by google cron task */
+    async function doSomething() {
+      await fetch('http://localhost:5000/')
+    }
+    doSomething()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     This is app
     </div>
   );
 }
